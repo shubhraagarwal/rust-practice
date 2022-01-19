38973d12e88413e591ee7f9b76971d914aef2108
shubhraagarwal <agarwalshubhra2002@gmail.com>
2022-01-19 22:06:08 +0530

    Practice day 1

---
+++ README.md
@@ -0,0 +1,3 @@
+This repository contains a few code snippets that i have created while learning rust
+
+If you want to refer to some dope notes, go check out this <a href="https://shubhraagarwal.notion.site/Rust-eb0d804cb64f4b4b8895ce906b3ccf98"> link </a>
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#[derive(Debug)]
+struct Test {
+  x: i64
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let test = Test { x: 5 };
+        println!("{:?} months in a year.", test);
+    println!("{1:?} {0:?} is the {actor:?} name.",
+             "Slater",
+             "Christian",
+             actor="actor's");
+    // let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+    // println!("too long tuple: {:?}", too_long_tuple);
 }
