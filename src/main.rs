bcdb5474c62cdc83817524b6af3400cc67d32d55
shubhraagarwal <agarwalshubhra2002@gmail.com>
2022-01-23 04:15:02 +0530

    Playing with variables in rust

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-#[derive(Debug)]
-struct Test {
-  x: i64
-}
-
+// The questions done in this repository can be found on https://github.com/rust-lang/rustlings/blob/main/exercises
+mod variables;
 
 fn main() {
-    let test = Test { x: 5 };
-        println!("{:?} months in a year.", test);
-    println!("{1:?} {0:?} is the {actor:?} name.",
-             "Slater",
-             "Christian",
-             actor="actor's");
+    // let test = Test { x: 5 };
+    //     println!("{:?} months in a year.", test);
+    // println!("{1:?} {0:?} is the {actor:?} name.",
+    //          "Slater",
+    //          "Christian",
+    //          actor="actor's");
     // let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
     // println!("too long tuple: {:?}", too_long_tuple);
+
+    variables::run();
 }
---
+++ src/variables.rs
@@ -0,0 +1,21 @@
+pub fn run() {
+    // !variables1.rs
+    // let x = 5;
+    // println!("x has the value {}", x);
+
+    //! variables2.rs
+    // let x : u8 = 10;
+    // if x == 10 {
+    //     println!("Ten!");
+    // } else {
+    //     println!("Not ten!");
+    // }
+    //! variables3.rs
+    // let mut x = 3;
+    // println!("Number {}", x);
+    // x = 5; // don't change this line
+    // println!("Number {}", x);
+    // !variables4.rs
+    let x: i32  =0;
+    println!("Number {}", x);
+}
