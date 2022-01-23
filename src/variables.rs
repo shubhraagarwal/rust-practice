67409a44785be90901dcaf823287d4cfdaebcdbc
shubhraagarwal <agarwalshubhra2002@gmail.com>
2022-01-23 18:44:58 +0530

    variables done

--- src/variables.rs
+++ src/variables.rs
@@ -1,30 +1,30 @@
 const NUMBER : i32 = 3;
 pub fn run() {
     // !variables1.rs
-    // let x = 5;
-    // println!("x has the value {}", x);
+    let x = 5;
+    println!("x has the value {}", x);
 
     //! variables2.rs
-    // let x : u8 = 10;
-    // if x == 10 {
-    //     println!("Ten!");
-    // } else {
-    //     println!("Not ten!");
-    // }
+    let x : u8 = 10;
+    if x == 10 {
+        println!("Ten!");
+    } else {
+        println!("Not ten!");
+    }
     //! variables3.rs
-    // let mut x = 3;
-    // println!("Number {}", x);
-    // x = 5; // don't change this line
-    // println!("Number {}", x);
+    let mut x = 3;
+    println!("Number {}", x);
+    x = 5; // don't change this line
+    println!("Number {}", x);
     // !variables4.rs
-    // let x: i32  =0;
-    // println!("Number {}", x);
+    let x: i32  =0;
+    println!("Number {}", x);
     //! variables5.rs
-    // let number = "T-H-R-E-E"; // don't change this line
-    // println!("Spell a Number : {}", number);
-    // //used the concept of shadowing here, when a varibale is re-initialized, it is not a new variable, it is a new value for the variable
-    // let number = 3;
-    // println!("Number plus two is : {}", number + 2);
+    let number = "T-H-R-E-E"; // don't change this line
+    println!("Spell a Number : {}", number);
+    //used the concept of shadowing here, when a varibale is re-initialized, it is not a new variable, it is a new value for the variable
+    let number = 3;
+    println!("Number plus two is : {}", number + 2);
     //! variables6.rs
     println!("Number {}", NUMBER);
 }
