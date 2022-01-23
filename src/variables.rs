7070df7a30aeaca0c5f58fd2cf80e6df55674fdc
shubhraagarwal <agarwalshubhra2002@gmail.com>
2022-01-23 18:43:12 +0530

    variables6

--- src/variables.rs
+++ src/variables.rs
@@ -1,3 +1,4 @@
+const NUMBER : i32 = 3;
 pub fn run() {
     // !variables1.rs
     // let x = 5;
@@ -19,9 +20,11 @@ pub fn run() {
     // let x: i32  =0;
     // println!("Number {}", x);
     //! variables5.rs
-    let number = "T-H-R-E-E"; // don't change this line
-    println!("Spell a Number : {}", number);
-    //used the concept of shadowing here, when a varibale is re-initialized, it is not a new variable, it is a new value for the variable
-    let number = 3;
-    println!("Number plus two is : {}", number + 2);
+    // let number = "T-H-R-E-E"; // don't change this line
+    // println!("Spell a Number : {}", number);
+    // //used the concept of shadowing here, when a varibale is re-initialized, it is not a new variable, it is a new value for the variable
+    // let number = 3;
+    // println!("Number plus two is : {}", number + 2);
+    //! variables6.rs
+    println!("Number {}", NUMBER);
 }
