50539c1c8091ad214f0d6448e3a44fecde861de8
shubhraagarwal <agarwalshubhra2002@gmail.com>
2022-01-23 18:41:53 +0530

    variables5 done

--- src/variables.rs
+++ src/variables.rs
@@ -16,6 +16,12 @@ pub fn run() {
     // x = 5; // don't change this line
     // println!("Number {}", x);
     // !variables4.rs
-    let x: i32  =0;
-    println!("Number {}", x);
+    // let x: i32  =0;
+    // println!("Number {}", x);
+    //! variables5.rs
+    let number = "T-H-R-E-E"; // don't change this line
+    println!("Spell a Number : {}", number);
+    //used the concept of shadowing here, when a varibale is re-initialized, it is not a new variable, it is a new value for the variable
+    let number = 3;
+    println!("Number plus two is : {}", number + 2);
 }
